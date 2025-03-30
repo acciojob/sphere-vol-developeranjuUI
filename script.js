609130aba88aaac30baad842c4f833ebbc89655b
developeranjuUI <116105555+developeranjuUI@users.noreply.github.com>
2025-03-30 23:19:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function volume_sphere() {
     //Write your code here
+	event.preventDefault();
+	let radius = parseFloat(document.getElementById('radius').value);
+	if(NaN(radius) || radius<0){
+		document.getElementById('volume').value = 'NaN';
+		return;
+	}
+	let volume = (4/3)*Math.PI*Math.pow(radius, 3);
+	document.getElementById('volume').value = volume.toFixed(4);
   
 } 
 
